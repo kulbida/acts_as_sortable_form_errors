e22bc7ab41a40f01c6f44feb44c45fe69e93470e
Bogdan Kulbida <bkulbida@gmail.com>
2012-06-20 10:19:27 +0300

    A patch.

--- lib/acts_as_sortable_form_errors.rb
+++ lib/acts_as_sortable_form_errors.rb
@@ -1,25 +1,25 @@
-module SimpleForm
+module Sortable::FormBuilder
 
-  class FormBuilder
+  def sortable_error_messages(options={})
+    options[:fields] ||= []
 
-    def error_messages(options={})
-      options[:fields] ||= []
+    return error_messages(options) unless options[:fields].size > 0
 
-      return super(options) unless options[:fields].size > 0
-
-      object = convert_to_model(objectify_options(options)[:object])
-      if object.errors.count > 0
-        ordered_error_messages = ::ActiveSupport::OrderedHash.new
-        options[:fields].each do |field|
-          ordered_error_messages.store(field, object.errors.messages[field])
-        end
-        object.errors.clear
-        object.errors.messages.merge!(ordered_error_messages)
-
-        @template.error_messages_for(object, objectify_options(options))
-      else
-        ''
+    object = convert_to_model(objectify_options(options)[:object])
+    if object.errors.count > 0
+      ordered_error_messages = ::ActiveSupport::OrderedHash.new
+      options[:fields].each do |field|
+      ordered_error_messages.store(field, object.errors.messages[field])
       end
+      object.errors.clear
+      object.errors.messages.merge!(ordered_error_messages)
+
+      @template.error_messages_for(object, objectify_options(options))
+     else
+     ''
     end
   end
+
 end
+
+ActionView::Helpers::FormBuilder.send(:include, Sortable::FormBuilder)
